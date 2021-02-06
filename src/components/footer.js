8b436dc09cf9ae125e4c71c6b519a9e410336234
Everett Lewis <everettlewis@Everetts-MacBook-Pro.local>
2021-02-06 12:31:24 -0600

    editing about page

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
     }}
   >
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>Copyright <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Everett Lewis</Link></p>
     </div>
   </footer>
 )
